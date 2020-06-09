3c213d98073195eb8d1ab48aa10cc1e6721e54f6
dytveerendra <63532874+dytveerendra@users.noreply.github.com>
2020-06-09 07:44:35 +0530

    added poi dependencies

--- src/main/java/com/dyt/utilities/ExcelLib.java
+++ src/main/java/com/dyt/utilities/ExcelLib.java
@@ -1,5 +1,13 @@
 package com.dyt.utilities;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 //============================================================
 /*
  * 
@@ -9,17 +17,38 @@ package com.dyt.utilities;
  * 
  */
 public class ExcelLib {		
-	
 	//============================================================
 	/*
 	 * 
 	 * 
 	 */
+	//====================Class properties=======================
+	static FileInputStream inpstr = null;
+	static XSSFWorkbook workbook = null;
+	
+	//===========================================================
 	public static String getCellValue(String filepath, String sheetname, int rowIndex, int colIndex)
 	{
 		String strCellValue = null;
+		try{	
+			
+			File file = new File(filepath);
+			inpstr = new FileInputStream(file);
+			workbook = new XSSFWorkbook(inpstr);					
+			
+			XSSFSheet worksheet = workbook.getSheet(sheetname);	
+			Row row = worksheet.getRow(rowIndex);
+			strCellValue = row.getCell(colIndex).getStringCellValue();										
+		}				
 		
+		catch(IOException e)	{
+			System.out.println("File not found or unable to read/write data..");
+		}
 		
+		catch(Exception e)	{
+			System.out.println(e.getMessage());
+			System.out.println("unknown exception");
+		}		
 		
 		return strCellValue;
 	}
