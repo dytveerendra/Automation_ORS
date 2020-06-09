0dedc109c0771b38abf956d9e2e6cc328580ac64
dytveerendra <63532874+dytveerendra@users.noreply.github.com>
2020-06-09 07:30:14 +0530

    created excellib,updated config

--- src/main/java/com/dyt/ors/config/Config.java
+++ src/main/java/com/dyt/ors/config/Config.java
@@ -7,7 +7,7 @@ public class Config {
 	public static WebDriver driver = null;
 	public static final String URL = "";
 	public static String Browser = "chrome";
-	public static String TestDataPath = "";
-	public static String ResultsPath = "";
+	public static String TestDataPath = System.getProperty("user.dir")+"//TestData";
+	public static String ResultsPath = System.getProperty("user.dir")+"//Results";
 
 }
---
+++ src/main/java/com/dyt/utilities/ExcelLib.java
@@ -0,0 +1,76 @@
+package com.dyt.utilities;
+
+//============================================================
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+public class ExcelLib {		
+	
+	//============================================================
+	/*
+	 * 
+	 * 
+	 */
+	public static String getCellValue(String filepath, String sheetname, int rowIndex, int colIndex)
+	{
+		String strCellValue = null;
+		
+		
+		
+		return strCellValue;
+	}
+	
+	//============================================================
+	public static String[] getRowData(String filepath, String sheetname, int colIndex, String cellValue)
+	{
+		String[] strRowData = null;
+		
+		
+		
+		return strRowData;
+	}
+	
+	//============================================================
+	public static String[] getRowData(String filepath, String sheetname, int rowIndex)
+	{
+		String[] strRowData = null;
+		
+		
+		
+		return strRowData;
+	}
+	
+	//============================================================	
+	public static void setCellValue(String filepath, String sheetname, int rowIndex, int colIndex)
+	{
+		
+		
+	}
+	
+	//============================================================
+	public static boolean compareSheets(String filepath, String sheet1, String sheet2)
+	{
+		boolean bResult = false;
+		
+		
+		
+		return bResult;
+	}
+	
+	//============================================================
+	public static boolean compareFiles(String file1, String file2)
+	{
+		boolean bResult = false;
+		
+		
+		
+		return bResult;
+	}
+	
+	//============================================================
+
+}
